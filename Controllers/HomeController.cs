3a3c1d8cfee46a7c630d61491b7a981ed348167c
Matthew Cullen <m23.cullen@gmail.com>
2021-02-17 14:23:39 -0500

    add HomeController Actions: Create, Welcome, Login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,15 +11,34 @@ namespace aspNetAuth.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
+        public HomeController(AppDbContext context) {
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(User user) {
+            if (ModelState.IsValid) {
+
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Welcome));
+            }
+            return View(user);
+        }
+
+        public IActionResult Welcome() {
+            return View();
+        }
+
+        public ViewResult Login() {
             return View();
         }
 
